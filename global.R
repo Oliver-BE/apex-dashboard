c9cbaa71b3fb28b481b0c674fabb85fe2d227ae3
Oliver-BE <obaldwinedwards@gmail.com>
2021-03-17 17:04:47 -0400

    small changes:

--- global.R
+++ global.R
@@ -1,16 +1,14 @@
 library(tidyverse)
 library(shiny)
 library(lubridate)
-library(googlesheets4)
+library(gsheet)
 
 # Initial code ----------------------------------------------------------------- 
-gs4_deauth()
-
-apex_df <- as.data.frame(read_sheet("https://docs.google.com/spreadsheets/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit?usp=sharing")) %>%
-  select(-c(...9, ...12, ...13, `Email Address`, `Game Number`,
-            `Select the only loba`)) %>%
+apex_df <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit#gid=13184181") %>%   
   drop_na() %>%
-  # mutate(Timestamp = parse_date_time(Timestamp, orders = "%m/%d/%y %H:%M:%S")) %>%
-  arrange(desc(Timestamp))
+  mutate(survival_time_dt = strptime(`Survival Time`, format = "%M:%S"),
+         `Survival Time (min)` = round(minute(survival_time_dt) + second(survival_time_dt) / 60, digits = 2)) %>% 
+  select(-c(survival_time_dt)) %>%
+  arrange(desc(Timestamp)) 
 
 data(apex_df)
--- scratch.R
+++ scratch.R
@@ -1,8 +1,8 @@
-apex_df <- as.data.frame(read_sheet("https://docs.google.com/spreadsheets/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit?usp=sharing")) %>%
-  select(-c(...9, ...12, ...13, `Email Address`, `Game Number`,
-            `Select the only loba`)) %>%
+apex_df <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit#gid=13184181") %>%   
   drop_na() %>%
-  mutate(Timestamp = parse_date_time(Timestamp, orders = "%m/%d/%y %H:%M:%S")) %>%
+  mutate(survival_time_dt = strptime(`Survival Time`, format = "%M:%S"),
+         `Survival Time` = minute(survival_time_dt) + second(survival_time_dt) / 60) %>% 
+  select(-c(survival_time_dt)) %>%
   arrange(desc(Timestamp))
 
 glimpse(apex_df)
