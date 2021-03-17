a1ec72310cd07cbe06259a22306faefab50aa6de
Oliver-BE <obaldwinedwards@gmail.com>
2021-03-17 13:04:18 -0400

    small

--- global.R
+++ global.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(shiny)
+library(googlesheets4)
 
 # Initial code ----------------------------------------------------------------- 
 apex_df <- as.data.frame(read_sheet("https://docs.google.com/spreadsheets/u/1/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit#gid=13184181")) %>% 
