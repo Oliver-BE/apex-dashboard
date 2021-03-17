7dc3a98d7d6e123b13794f8763afde6b16e35f25
Oliver-BE <obaldwinedwards@gmail.com>
2021-03-17 13:09:23 -0400

    deauth

--- global.R
+++ global.R
@@ -3,7 +3,9 @@ library(shiny)
 library(googlesheets4)
 
 # Initial code ----------------------------------------------------------------- 
-apex_df <- as.data.frame(read_sheet("https://docs.google.com/spreadsheets/u/1/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit#gid=13184181")) %>%
+gs4_deauth()
+
+apex_df <- as.data.frame(read_sheet("https://docs.google.com/spreadsheets/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit?usp=sharing")) %>%
  select(-c(...9, ...11, `Email Address`, `Game Number`))
 
 data(apex_df)
--- scratch.R
+++ scratch.R
@@ -1,4 +1,4 @@
-apex_df <- as.data.frame(read_sheet("https://docs.google.com/spreadsheets/u/1/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit#gid=13184181")) %>% 
+apex_df <- as.data.frame(read_sheet("https://docs.google.com/spreadsheets/d/1v40AgpaoRrA3v5eEjztB5Rw_OGKRHw3L56hhJdzdqAs/edit?usp=sharing")) %>% 
   select(-c(...9, ...11, `Email Address`, `Game Number`))  %>% 
   drop_na()
 
